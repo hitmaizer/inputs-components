59b8a4e2027aa1c5a4c8c5235578a14a9de79955
hitmaizer <jose.sousalves@gmail.com>
2022-02-10 14:38:48 +0000

    added theme for theme provider

---
+++ src/theme/Theme.js
@@ -0,0 +1,14 @@
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+
+export default {
+    colors: {
+        default: "#828282",
+        defaultHover: "#333333",
+        defaultFocus: "#2962FF",
+        danger: "#D32F2F",
+        disabled: "#F2F2F2",
+        
+
+
+    }
+}
