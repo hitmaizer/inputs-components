922419e32eb90d6fdf07e1ad7e1e49b64a5463f8
hitmaizer <jose.sousalves@gmail.com>
2022-02-10 18:48:17 +0000

    jsx presentation markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import IconInput from './components/IconInput'
+import EndIconInput from './components/EndIconInput'
 import { ThemeProvider } from 'styled-components'
 import Theme from './theme/Theme'
 import DefaultInput from './components/DefaultInput'
 import ErrorInput from './components/ErrorInput'
 import DisabledInput from './components/DisabledInput'
 import TextInput from './components/TextInput'
+import StyledSpan from './elements/StyledSpan'
+import StartIconInput from './components/StartIconInput'
+import Input from './elements/StyledInputs'
+import Label from './elements/StyledLabel'
 
 
 
@@ -15,6 +19,7 @@ export default function App() {
         "<Input error />",
         "<Input disabled />",
         "<Input helperText=”Some interesting text” />",
+        "<Input helperText=”Some interesting text” error />",
         "<Input startIcon />",
         "<Input endIcon />",
         "<Input value=”text” />",
@@ -33,10 +38,63 @@ export default function App() {
                 <ErrorInput />
                 <h1 className="button__title">{titles[2]}</h1>
                 <DisabledInput />
-                <h1 className="button__title">{titles[3]}</h1>
-                <TextInput sublabel />
-                <IconInput sublabel helperText={"Ola mate"} endIcon/>
+                <div className="item--rows flex-row">
+                    <div className="flex-col">
+                        <h1 className="button__title">{titles[3]}</h1>
+                            <div className="item__wrapper">
+                            <TextInput sublabel />
+                            <StyledSpan className="input__sublabel">Some Interesting Text</StyledSpan>
+                            </div>
+                    </div>
+                    <div className="item--rows flex-col">
+                        <div className="flex-col">
+                            <h1 className="button__title">{titles[4]}</h1>
+                                <div className="item__wrapper">
+                                <TextInput sublabel />
+                                <StyledSpan error className="input__sublabel">Some Interesting Text</StyledSpan>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="item--rows flex-row">
+                    <div className="item__wrapper">
+                        <h1 className="button__title">{titles[5]}</h1>
+                        <StartIconInput startIcon />
+                    </div>
+                    <div className="item__wrapper">
+                        <h1 className="button__title">{titles[6]}</h1>
+                        <EndIconInput endIcon/>
+                    </div>
+                </div>
+                <div className="item__wrapper flex-col">
+                    <h1 className="button__title">{titles[7]}</h1>
+                    <Label className="input__label">Label</Label>
+                    <Input value="Text" placeholder="Placeholder"/>
+                </div>
+                <div className="item--rows flex-row">
+                    <div className="item__wrapper flex-col">
+                        <h1 className="button__title">{titles[8]}</h1>
+                        <Label className="input__label">Label</Label>
+                        <Input sm placeholder="Placeholder"/>
+                    </div>
+                    <div className="item__wrapper flex-col">
+                        <h1 className="button__title">{titles[9]}</h1>
+                        <Label className="input__label">Label</Label>
+                        <Input md placeholder="Placeholder"/>
+                    </div>
+                </div>
+                <div className="item__wrapper flex-col">
+                    <h1 className="button__title">{titles[10]}</h1>
+                    <Label className="input__label">Label</Label>
+                    <Input fullWidth placeholder="Placeholder"/>
+                </div>
+                <div className="item__wrapper flex-col">
+                    <h1 className="button__title">{titles[11]}</h1>
+                    <Label className="input__label">Label</Label>
+                    <textarea rows="4" className="text__area"></textarea>
+                </div>
             </div>
+            <footer className="footer__sign">created by <b><u>Jose Alves</u></b> - devChallenges.io</footer>
         </ThemeProvider>
     )
 }
