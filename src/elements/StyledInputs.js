291fc8fd927a0de36b00b18e402182ba0f6fa94b
hitmaizer <jose.sousalves@gmail.com>
2022-02-10 12:59:58 +0000

    styling of default input

--- src/elements/StyledInputs.js
+++ src/elements/StyledInputs.js
@@ -1,9 +1,18 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export default styled.input`
     height: 56px;
     color: #828282;
     border: solid 1px #828282;
-    
+    border-radius: 8px;
+    font-family: 'Noto Sans Display', sans-serif;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 20px;
+    width: 200px;
+
+    ${(props) => props.default && css `
+        
+    `}
   
 `;
