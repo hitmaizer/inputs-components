959cb670e4ca49c12ba64711a25ba13c24b95046
hitmaizer <jose.sousalves@gmail.com>
2022-02-10 12:48:55 +0000

    input wrapper + label

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,8 +4,8 @@ import StyledInput from '../elements/StyledInputs'
 export default function Input() {
     return (
         <div className="input__wrapper flex-col">
-            <label className="input__label" htmlFor="">Label</label>
-            <StyledInput />
+            <label htmlFor="" className="input__label">Label</label>
+           <StyledInput />
         </div>
     )
 }
