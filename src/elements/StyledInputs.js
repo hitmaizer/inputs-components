34fbb1b2992eef645d2f7dff2e3a44c2dbd468ed
hitmaizer <jose.sousalves@gmail.com>
2022-02-10 14:38:17 +0000

    added default and error state

--- src/elements/StyledInputs.js
+++ src/elements/StyledInputs.js
@@ -2,8 +2,8 @@ import styled, { css } from 'styled-components';
 
 export default styled.input`
     height: 56px;
-    color: #828282;
-    border: solid 1px #828282;
+    color: ${(props) => props.theme.colors.default};
+    border: solid 1px ${(props) => props.theme.colors.default};
     border-radius: 8px;
     font-family: 'Noto Sans Display', sans-serif;
     font-weight: 500;
@@ -12,7 +12,30 @@ export default styled.input`
     width: 200px;
 
     ${(props) => props.default && css `
-        
+        &:hover {
+            border: solid 1px ${props.theme.colors.defaultHover};
+            color: ${props.theme.colors.defaultHover};
+        }
+
+        &:focus, :focus-visible {
+            border: solid 1px ${props.theme.colors.defaultFocus};
+        }
+
+    `}
+    
+    ${(props) => props.error && css `
+        border: solid 1px ${props.theme.colors.danger};
+
+
+        &:hover {
+            color: ${props.theme.colors.danger};
+            border: solid 1px ${props.theme.colors.defaultHover};
+        }
+
+        &:focus, :focus-visible {
+            border: solid 1px ${props.theme.colors.danger};
+        }
+
     `}
   
 `;
